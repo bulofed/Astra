839372a80c5e68ad5a8e25b3d9eb540197a62aff
BUTOR Loic <loic.butor@etu.univ-littoral.fr>
2024-01-24 00:05:03 +0100

    Player Sprite & Animations

--- player.py
+++ player.py
@@ -5,6 +5,16 @@ class Player:
     def __init__(self, game):
         self.game = game
         self.x, self.y, self.z = PLAYER_POS
+        self.current_frame = 0
+        self.animation_time = 0
+        self.frame_duration = .5
+        self.load_sprites()
+    
+    def load_sprites(self):
+        self.sprites = []
+        self.sprites.extend(
+            pg.image.load(f'images/player/idle_d{i}.png') for i in range(1, 3)
+        )
         
     def movement(self):
         dx, dy, dz = 0, 0, 0
@@ -29,10 +39,16 @@ class Player:
         
     def update(self):
         self.movement()
+        self.animation_time += self.game.delta / 1000
+        if self.animation_time >= self.frame_duration:
+            self.animation_time -= self.frame_duration
+            self.current_frame = (self.current_frame + 1) % len(self.sprites)
             
     def draw(self):
         x, y = self.game.map.calculate_isometric_position(self.x, self.y, self.z, self.game.camera.zoom)
-        pg.draw.circle(self.game.screen, 'red', (x - self.game.camera.x, y - self.game.camera.y), 10)
+        sprite = self.sprites[self.current_frame]
+        sprite_resized = pg.transform.scale(sprite, (SPRITE_WIDTH * self.game.camera.zoom, SPRITE_HEIGHT * self.game.camera.zoom))
+        self.game.screen.blit(sprite_resized, (x - self.game.camera.x, y - self.game.camera.y))
         
     @property
     def position(self):
