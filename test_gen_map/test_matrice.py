c31a372dc49330223da505aefc3cabecb012a1e6
DUVET Alexandre <alexandre.duvet@etu.univ-littoral.fr>
2024-01-26 09:42:54 +0100

    test map

--- test_gen_map/test_matrice.py
+++ test_gen_map/test_matrice.py
@@ -1,4 +1,8 @@
 import random
+import json
+
+
+
 
 def creer_matrice(taille):
     matrice_jeu = []
@@ -29,7 +33,15 @@ def creer_map():
     
     
 taille = 6
-level = creer_level()
-print(level)
+# level = creer_level()
+# print(level)
 # niveaux = creer_map()
 # print(niveaux)
+
+json_stock = "./maps/test.json"
+
+with open(json_stock,"w") as f:
+    for i in range(1):
+        map_data = creer_map()
+        json.dump(map_data, f, indent=1)
+        f.write('\n')
